ad597648ceca5712b852b58cdc8483314a0b031f
Nisam <nisam8104@gmail.com>
2023-02-05 21:58:17 +0530

    react navigation installed

--- App.js
+++ App.js
@@ -2,8 +2,25 @@ import React, {useState} from 'react';
 import {Button, StyleSheet, View} from 'react-native';
 import Factpage from './src/pages/Factpage';
 import Breedspage from './src/pages/Breedspage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const App = () => {
+const Tab = createBottomTabNavigator();
+
+const TabScreen = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Facts" component={Factpage} />
+    <Tab.Screen name="Breeds" component={Breedspage} />
+  </Tab.Navigator>
+);
+
+export default function App() {
+  return (
+    <NavigationContainer><TabScreen/></NavigationContainer>
+  );
+}
+
+const Apps = () => {
   const [page, setPage] = useState('facts');
   return (
     <View style={styles.appContainer}>
@@ -28,4 +45,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+// export default App;
