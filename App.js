c8e99dcfaf953419a50d04756aa295d94ae42522
Nisam <nisam8104@gmail.com>
2023-02-11 00:17:40 +0530

    tab,stack navigation working

--- App.js
+++ App.js
@@ -1,51 +1,17 @@
-import React, {useState} from 'react';
-import {Button, StyleSheet, View} from 'react-native';
-import Factpage from './src/pages/Factpage';
-import Breedspage from './src/pages/Breedspage';
+import React from 'react';
+import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Breedspage1 from './src/pages/Breedspage1';
-
-const Tab = createBottomTabNavigator();
-
-const TabScreen = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Breeds" component={Breedspage1} />
-    <Tab.Screen name="Facts" component={Factpage} />
-  </Tab.Navigator>
-);
-
+import MainNavigation from './src/navigation/MainNavigation';
 export default function App() {
   return (
     <NavigationContainer>
-      <TabScreen />
+      <MainNavigation />
     </NavigationContainer>
   );
 }
-
-const Apps = () => {
-  const [page, setPage] = useState('facts');
-  return (
-    <View style={styles.appContainer}>
-      {page === 'facts' ? <Factpage /> : null}
-      {page === 'breeds' ? <Breedspage1 /> : null}
-      {/* <View style={styles.buttonsContainer}>
-        <Button title="Facts" onPress={() => setPage('facts')} />
-        <Button title="Breeds" onPress={() => setPage('breeds')} />
-      </View> */}
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
   },
-  buttonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
 });
-
-// export default App;
