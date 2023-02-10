f897594c50c880d4eb4960d977418037b212f6e5
Nisam <nisam8104@gmail.com>
2023-02-11 01:32:54 +0530

    added images in tab icon

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -2,13 +2,31 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Factpage from '../pages/Factpage';
 import BreedStack from './BreedStack';
+import { Image } from 'react-native';
 
 
 const Tab = createBottomTabNavigator();
 const TabScreen = () => (
     <Tab.Navigator>
-      <Tab.Screen  options={{headerShown: false}} name="Breedstack" component={BreedStack} />
-      <Tab.Screen name="Facts" component={Factpage} />
+      <Tab.Screen 
+       options={{
+        tabBarLabel:'Breeds',
+        headerShown: false,
+       tabBarIcon: ({ focused, color, size }) => (
+        <Image
+          source={require('../images/happy.png')}
+          style={{ tintColor: color, width: size, height: size }}/>)}}
+       name="Breedstack" 
+       component={BreedStack} />
+      <Tab.Screen 
+      options={{
+        tabBarLabel:'Fact',
+        
+       tabBarIcon: ({ focused, color, size }) => (
+        <Image
+          source={require('../images/did-you-know.png')}
+          style={{ width: size, height: size }}/>)}}
+      name="Facts" component={Factpage} />
     </Tab.Navigator>
   );
   export default TabScreen;
